fe6e7de079ecd0cd653b58a8edb451705453c72c
nlin <nlin@thoughtworks.com>
2017-08-10 22:53:38 +0800

    created a new project with bake

---
+++ app/Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => true,
+		'host' => 'localhost',
+		'port' => 8889,
+		'login' => 'root',
+		'password' => 'root',
+		'database' => 'cakephp-boilerplate',
+	);
+}
